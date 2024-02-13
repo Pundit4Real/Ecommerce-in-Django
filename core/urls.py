cc89f7419dab0192f85293d17f99f8beebd0b02c
Mohammed Ali <mohammedaalli088@gmail.com>
2024-02-13 05:23:24 +0000

    feat: core.views added

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("", views.index,name="index"),
     path("products/", views.product_list_view,name="product-list"),
     path("category/", views.category_list_view,name="category-list"),
-    path("category/<cid>", views.category_product_list_view,name="category-product-list"),
+    path("category/<cid>/", views.category_product_list_view,name="category-product-list"),
 ]
