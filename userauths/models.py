fe440627619f7bc0b78f508e95eaf8e83b4d8485
Mohammed Ali <mohammedaalli088@gmail.com>
2024-03-31 12:28:52 +0000

    feat: signals for user profile creation started

--- userauths/models.py
+++ userauths/models.py
@@ -1,6 +1,7 @@
 from enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
@@ -25,3 +26,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile,sender=User)
+post_save.connect(save_user_profile, sender=User)
