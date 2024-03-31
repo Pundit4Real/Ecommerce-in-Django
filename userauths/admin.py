0f6a69f229ee9fd9e32d01e2b3c92e14d45679ca
Mohammed Ali <mohammedaalli088@gmail.com>
2024-03-31 08:53:53 +0000

    feat: user profile models created

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from userauths.models import User
+from userauths.models import User,Profile
 
 class UserAdmin(admin.ModelAdmin):
     list_display= ['username','email','bio']
+class ProfileAdmin(admin.ModelAdmin):
+    list_display= ['full_name','bio','phone','image']
 
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile,ProfileAdmin)
--- userauths/models.py
+++ userauths/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username= models.CharField(max_length=100)
-    bio = models.CharField(max_length=200)
+    bio = models.CharField(max_length=200,null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
@@ -14,3 +14,14 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    image = models.ImageField(upload_to="profile-image")
+    full_name = models.CharField(max_length=200)
+    bio = models.CharField(max_length=200, null=True,blank=True)
+    phone = models.CharField(max_length=200)
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.full_name
